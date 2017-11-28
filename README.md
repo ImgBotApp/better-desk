dad556583cf0eb811ce6ab36a7c1e87253166480
Chris Guy <christien.guy@theunit.co.uk>
2017-11-28 00:36:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+The task is the build out the page in the provided sketch file.
+
+This is a starter template with a gulpfile setup to compile SASS and move js and html into the correct directories. 
+
+You can start the dev server by running `gulp serve` in the root of the project.
+
+The search bar will need to expand horizontally when selected. 
+
+The "Why BetterDesk" section has expandable sections on the left. These behave a lot like an 'accordion'. 
+
+There are many to choose from, to find one that will work I recommend [http://youmightnotneedjqueryplugins.com/](http://youmightnotneedjqueryplugins.com/) or [https://plainjs.com/](https://plainjs.com/)
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+
+const browserSync = require("browser-sync").create();
+
+gulp.task("serve", ["sass", "js", "templates"], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./public"
+    }
+  });
+
+  gulp.watch("./src/sass/**/*.scss", ["sass"]);
+  gulp.watch("./src/js/**/*.js", ["js"]);
+  gulp.watch("./src/templates/**/*.html", ["templates"]);
+});
+
+gulp.task("sass", function() {
+  return gulp
+    .src("./src/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./public/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("js", function() {
+  return gulp
+    .src("./src/js/*.js")
+    .pipe(gulp.dest("./public/js"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("templates", function() {
+  return gulp
+    .src("./src/templates/*.html")
+    .pipe(gulp.dest("./public"))
+    .pipe(browserSync.stream());
+});
