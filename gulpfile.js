d8b87c4c4da5c2996006c6210f34cf786829b8b9
ambrwlsn <arw89@uclive.ac.nz>
2017-11-28 14:33:20 +0000

    added img folder, altered gulp file to manage these, added svgs, used flexbox to position elements

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require("gulp-sass");
 
 const browserSync = require("browser-sync").create();
 
-gulp.task("serve", ["sass", "js", "templates"], function() {
+gulp.task("serve", ["sass", "js", "images", "templates"], function() {
   browserSync.init({
     server: {
       baseDir: "./public"
@@ -13,6 +13,7 @@ gulp.task("serve", ["sass", "js", "templates"], function() {
   gulp.watch("./src/sass/**/*.scss", ["sass"]);
   gulp.watch("./src/js/**/*.js", ["js"]);
   gulp.watch("./src/templates/**/*.html", ["templates"]);
+  gulp.watch("./src/img/**", ["images"]);
 });
 
 gulp.task("sass", function() {
@@ -27,6 +28,13 @@ gulp.task("js", function() {
   return gulp
     .src("./src/js/*.js")
     .pipe(gulp.dest("./public/js"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("images", function() {
+  return gulp
+    .src("./src/img/**")
+    .pipe(gulp.dest("./public/img"))
     .pipe(browserSync.stream());
 });
 
