0d0decc9969fe69420a63911687829d1673cf4e6
Amber <arw89@uclive.ac.nz>
2017-12-05 12:02:00 +0000

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,16 @@
 var whyItem = document.getElementsByClassName('why-box');
-    var whyHeading = document.getElementsByClassName('why-box-heading');
-    for (i = 0; i < whyHeading.length; i++) {
-        whyHeading[i].addEventListener('click', toggleItem, false);
-    }
-    function toggleItem() {
-        var itemClass = this.parentNode.className;
-        for (i = 0; i < whyItem.length; i++) {
-            whyItem[i].className = 'why-box close';
-        }
-        if (itemClass == 'why-box close') {
-            this.parentNode.className = 'why-box open';
-        }
-    }
+var whyHeading = document.getElementsByClassName('why-box-heading');
+    
+for (i = 0; i < whyHeading.length; i++) {
+  whyHeading[i].addEventListener('click', toggleItem, false);
+}
+
+function toggleItem() {
+  var itemClass = this.parentNode.className;
+  for (i = 0; i < whyItem.length; i++) {
+    whyItem[i].className = 'why-box close';
+  }
+  if (itemClass == 'why-box close') {
+    this.parentNode.className = 'why-box open';
+  }
+}
